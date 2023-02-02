09903cef8e4717c184626975013f7dc506e49445
vinaysripelli <vinaysripelli@gmail.com>
2023-02-02 21:26:31 +0530

    read params

--- src/pipeline_1.py
+++ src/pipeline_1.py
@@ -3,13 +3,21 @@ import argparse
 import yaml # define the configurations
 import logging
 
-
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config=yaml.safe_load(yaml_file)
+    return config
+def main(config_path,data_source):
+    config = read_params(config_path)
+    print(config)
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config",default="default") #configuration files consists of the all the parameters required
+    default_config_path= os.path.join("config","params.yaml")
+    args.add_argument("--config",default=default_config_path) #configuration files consists of the all the parameters required
     args.add_argument("--datasource",default=None)
     parsed_args= args.parse_args()
     #print(parsed_args)
-    print(parsed_args.config)
-    print(parsed_args.datasource)
+    #print(parsed_args.config)
+    #print(parsed_args.datasource)
+    main(config_path=parsed_args.config,data_source=parsed_args.datasource)
